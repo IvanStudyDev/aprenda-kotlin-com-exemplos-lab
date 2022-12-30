1359057f12ef6a2dbcc2b14fb9d1418088cec0bb
Ivan Ferreira Coelho <95661680+IvanStudyDev@users.noreply.github.com>
2022-12-29 23:37:59 -0300

    PRIMEIRA ALTERAÇÃO

--- desafio.kt
+++ desafio.kt
@@ -1,7 +1,7 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
-
+// TEM MUITO A EVOLUIR AINDA, E JUNTO COM ISSO VOU EVOLUIR ESSE CODIGO. 
 class Usuario
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
@@ -11,11 +11,15 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+         
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val inscritos1 = "Nome do aluno: Ivan, e sua formação: Kotlin!"
+        println("$inscritos1")
+   	val inscritos2 = "Nome do aluno: Pedro, e sua formação: Kotlin!"
+        println("$inscritos2")
+    val inscritos3 = "Nome do aluno: Paulo, e sua formação: Kotlin!"
+        println("$inscritos3")
 }
